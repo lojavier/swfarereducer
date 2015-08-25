8cfc15affc8ce5d9fa05f43b59f7eb5223a91361
Lorenzo Javier <lorenzo@ubuntu-14.04>
2015-08-25 01:03:11 -0700

    update

--- newflight.php
+++ newflight.php
@@ -16,50 +16,22 @@
 <?php
 // require_once "config.php";
 
-// $data = array('8ABYGC', 'Lorenzo', 'Javier');
-
-// $result = shell_exec('/usr/bin/python /opt/lampp/htdocs/swfarereducer2/sw_flight_validator.py ' . escapeshellarg(json_encode($data)) . ' > result.txt');
-
-// // Decode the result
-// $resultData = json_decode($result, true);
-
-// // This will contain: array('status' => 'Yes!')
-// var_dump($resultData);
-
-// $command = escapeshellcmd('/usr/bin/python /opt/lampp/htdocs/swfarereducer2/sw_flight_validator.py 8ABYGC Lorenzo Javier 2>&1');
-// $output = shell_exec($command);
-// echo $output . "<br><br>";
-
-// $command = escapeshellcmd('/opt/lampp/htdocs/swfarereducer2/sw_flight_validator.sh');
-// $output = shell_exec($command);
-// echo $output;
-
-// $command = "/usr/bin/wget -O 'change-air-reservation-results.html' --post-data='confirmationNumber=8ABYGC&firstName=LORENZO&lastName=JAVIER&submit=submit' https://www.southwest.com/flight/change-air-reservation.html 2>&1";
-$command = "/usr/bin/wget -O 'change-air-reservation.html' --save-cookies=cookie --post-data='confirmationNumber=".$_POST['CONFIRMATION_NUM']."&firstName=".$_POST['FIRST_NAME']."&lastName=".$_POST['LAST_NAME']."&submit=submit' https://www.southwest.com/flight/change-air-reservation.html 2>&1";
+$command = "/usr/bin/wget -O 'change-air-reservation-results.html' --save-cookies=cookie --post-data='confirmationNumber=".$_POST['CONFIRMATION_NUM']."&firstName=".$_POST['FIRST_NAME']."&lastName=".$_POST['LAST_NAME']."&submit=submit' https://www.southwest.com/flight/change-air-reservation.html 2>&1";
 echo $command . "<br>";
-// $output = shell_exec($command);
-// echo $output;
-
 exec($command, $output, $return);
 print_r($output);
 echo "<br>";
 print_r($return);
 echo "<br>";
-// $command = "ifconfig";
-// $output = shell_exec($command);
-// echo $output;
 
-// exec("/usr/bin/python /opt/lampp/htdocs/swfarereducer2/sw_flight_validator.py 8ABYGC Lorenzo Javier 2>&1", $output, $return);
-// print_r($output);
-
-// $file = popen($command,"r");
-// while( !feof( $file ) )
-// {
-// 	echo fread($file, 2048);
-// 	flush();
-// 	ob_flush();
-// }
-// pclose($file);
+$file = popen("change-air-reservation-results.html","r");
+while( !feof( $file ) )
+{
+	echo fread($file, 2048);
+	flush();
+	ob_flush();
+}
+pclose($file);
 
 ?>
 
