a2cfd4eb0afeab1da7225e98cf10a4dae353642a
lojavier <loj90@sbcglobal.net>
2015-12-18 17:34:53 -0800

    Update 12/18/2015

--- sw_fare_reducer.py
+++ sw_fare_reducer.py
@@ -69,13 +69,7 @@ def main():
 			RIGHT JOIN RESERVED_FLIGHTS \
 			ON UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID=RESERVED_FLIGHTS.UPCOMING_FLIGHT_ID \
 			GROUP BY RESERVED_FLIGHTS.CONFIRMATION_NUM \
-			ORDER BY UPCOMING_FLIGHTS.DEPART_DATE_TIME DESC,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE ASC,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE ASC"
-	# sql = "SELECT DISTINCT RESERVED_FLIGHTS.CONFIRMATION_NUM,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE,DATE_FORMAT(UPCOMING_FLIGHTS.DEPART_DATE_TIME, '%Y-%m-%d') AS A,DATE_FORMAT(UPCOMING_FLIGHTS.DEPART_DATE_TIME, '%Y-%m-%d') AS B,GROUP_CONCAT(DATE_FORMAT(UPCOMING_FLIGHTS.DEPART_DATE_TIME, '%Y-%m-%d')) \
-	# 		FROM UPCOMING_FLIGHTS \
-	# 		RIGHT JOIN RESERVED_FLIGHTS \
-	# 		ON UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID=RESERVED_FLIGHTS.UPCOMING_FLIGHT_ID \
-	# 		GROUP BY RESERVED_FLIGHTS.CONFIRMATION_NUM \
-	# 		ORDER BY UPCOMING_FLIGHTS.DEPART_DATE_TIME ASC"
+			ORDER BY UPCOMING_FLIGHTS.DEPART_DATE_TIME ASC,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE ASC,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE ASC"
 	try:
 		cursor.execute(sql)
 		results = cursor.fetchall()
@@ -100,10 +94,6 @@ def main():
 			else:
 				departDate = row[2].split(',')[0]
 				returnDate = False
-
-			print departAirportCode1,arriveAirportCode1,departAirportCode2,arriveAirportCode2,departDate,returnDate
-			print departAirportCode1,departAirportCode2,departDate
-			print arriveAirportCode1,arriveAirportCode2,returnDate
 
 			i = 0
 			departFlag = False
@@ -149,47 +139,41 @@ def main():
 						flightSearchArray.append([departAirportCode1,departAirportCode2,departDate,False])
 				else:
 					flightSearchArray.append([departAirportCode1,departAirportCode2,departDate,False])
-			print ""
+
 		flightSearchArray = sorted(flightSearchArray, key=lambda x: (x[2], x[0], x[1], x[3]), reverse=False)
-		print len(flightSearchArray)
-		for i in range(0,len(flightSearchArray))
-			for x in flightSearchArray:
-				departAirportCode = x[0]
-				arriveAirportCode = x[1]
-				departDate = x[2]
-				print x
-				if x[1] == "OAK":
-					flightSearchArray.remove(x)
-			
-		print ""
+		i = 0
+		while i < len(flightSearchArray):
+			if not flightSearchArray[i][3]:
+				j = 0
+				while j < len(flightSearchArray):
+					if flightSearchArray[i][1] == flightSearchArray[j][0] and flightSearchArray[i][0] == flightSearchArray[j][1] and datetime.datetime.strptime(flightSearchArray[j][2], "%Y-%m-%d") > datetime.datetime.strptime(flightSearchArray[i][2], "%Y-%m-%d"):
+						flightSearchArray[i][3] = flightSearchArray[j][2]
+						flightSearchArray.pop(j)
+						i = i - 1
+						break
+					elif j == len(flightSearchArray)-1:
+						flightSearchArray[i][3] = ( datetime.datetime.strptime(flightSearchArray[i][2], "%Y-%m-%d") + datetime.timedelta(days=1) ).strftime("%Y-%m-%d")
+					j = j + 1
+			i = i + 1
+	
 		for x in flightSearchArray:
-			print x
-
-	except:
-		return 1
-
-	return 1
-
-	sql = "SELECT DISTINCT UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE,DATE_FORMAT(UPCOMING_FLIGHTS.DEPART_DATE_TIME, '%Y-%m-%d') \
-			FROM UPCOMING_FLIGHTS \
-			RIGHT JOIN RESERVED_FLIGHTS \
-			ON UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID=RESERVED_FLIGHTS.UPCOMING_FLIGHT_ID"
-	try:
-		cursor.execute(sql)
-		results = cursor.fetchall()
-		for row in results:
-			departAirportCode = row[0]
-			arriveAirportCode = row[1]
-			departDate = row[2]
-			returnDate = ( datetime.datetime.strptime(departDate, "%Y-%m-%d") + datetime.timedelta(days=1) ).strftime("%Y-%m-%d")
-			flightSearch = "python sw_flight_search.py %s %s %s %s" % (departAirportCode,arriveAirportCode,departDate,returnDate)
-			os.system(flightSearch)
+			departAirportCode = x[0]
+			arriveAirportCode = x[1]
+			departDate = x[2]
+			returnDate = x[3]
+			try:
+				flightSearch = "python sw_flight_search.py %s %s %s %s" % (departAirportCode,arriveAirportCode,departDate,returnDate)
+				os.system(flightSearch)
+			except:
+				logF = open(logFile, "a")
+				logMessage = "%s ERROR: Unable to execute sw_flight_search.py [depart:%s|arrive:%s|date:%s|return:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),departAirportCode,arriveAirportCode,departDate,returnDate)
+				logF.write(logMessage)
+				logF.close()
 	except:
 		logF = open(logFile, "a")
-		logMessage = "%s ERROR: Unable to select distinct flights\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
+		logMessage = "%s ERROR: Unable to select distinct reservations by confirmation number\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
 		logF.write(logMessage)
 		logF.close()
-		db.close()
 		return 1
 
 	sql = "SELECT RESERVED_FLIGHTS.RESERVED_FLIGHT_ID,RESERVED_FLIGHTS.EMAIL,RESERVED_FLIGHTS.PHONE_NUM,WIRELESS_CARRIERS.CARRIER_TEXT_EMAIL,RESERVED_FLIGHTS.CONFIRMATION_NUM,RESERVED_FLIGHTS.FARE_LABEL,RESERVED_FLIGHTS.FARE_PRICE_PAID,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE,UPCOMING_FLIGHTS.DEPART_DATE_TIME,UPCOMING_FLIGHTS.FLIGHT_NUM,UPCOMING_FLIGHTS.FARE_PRICE_DOLLARS,UPCOMING_FLIGHTS.FARE_PRICE_POINTS \
@@ -248,7 +232,7 @@ def main():
 					logF.close()
 	except:
 		logF = open(logFile, "a")
-		logMessage = "%s ERROR: Unable to select distinct flights\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
+		logMessage = "%s ERROR: Unable to select distinct reserved flights for price comparison\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
 		logF.write(logMessage)
 		logF.close()
 		db.close()
--- sw_flight_checkin.py
+++ sw_flight_checkin.py
@@ -43,15 +43,19 @@ def sendCheckinAlert(notificationAddress,confirmationNum,departAirportCode,arriv
 		return 1
 
 checkinAlertMinutes = 30
-currentDateTime = datetime.datetime.strptime(time.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
-checkinAlertDateTime = datetime.datetime.strptime(time.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=24,minutes=checkinAlertMinutes)
+utc_datetime = datetime.datetime.utcnow()
+currentUtcDateTime = datetime.datetime.strptime(utc_datetime.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
+checkinAlertDateTime = datetime.datetime.strptime(utc_datetime.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=24,minutes=checkinAlertMinutes)
+
+print utc_datetime
+print currentUtcDateTime
+print checkinAlertDateTime
+print ""
 
 #####################################################################
 ## Set directory path and file name for response & results html file
 #####################################################################
 cwd = os.getcwd()
-# responseFile = cwd+"/logs/lookup-air-reservation.html"
-# resultsFile = cwd+"/logs/view-air-reservation.html"
 logFile = cwd+"/logs/"+time.strftime("%Y_%m_%d")+"_sw_flight_checkin.log"
 
 ## ADD timezone feature ****************
@@ -59,21 +63,86 @@ logFile = cwd+"/logs/"+time.strftime("%Y_%m_%d")+"_sw_flight_checkin.log"
 
 def main():
 	#####################################################################
-	## Retrieve list of upcoming reserved flights
+	## Delete past flights 
 	#####################################################################
 	db = MySQLdb.connect("127.0.0.1","root","swfarereducer","SWFAREREDUCERDB")
 	cursor = db.cursor()
-	sql = "SELECT RESERVED_FLIGHTS.RESERVED_FLIGHT_ID,RESERVED_FLIGHTS.EMAIL,RESERVED_FLIGHTS.PHONE_NUM,WIRELESS_CARRIERS.CARRIER_TEXT_EMAIL,RESERVED_FLIGHTS.CONFIRMATION_NUM,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE,UPCOMING_FLIGHTS.DEPART_DATE_TIME,UPCOMING_FLIGHTS.FLIGHT_NUM \
+	sql = "SELECT GROUP_CONCAT(RESERVED_FLIGHTS.RESERVED_FLIGHT_ID),GROUP_CONCAT(UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID) \
+			FROM RESERVED_FLIGHTS \
+			LEFT JOIN UPCOMING_FLIGHTS \
+			ON UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID=RESERVED_FLIGHTS.UPCOMING_FLIGHT_ID \
+			LEFT JOIN AIRPORTS \
+			ON UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE=AIRPORTS.AIRPORT_CODE \
+			WHERE CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Aruba','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Atlantic Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/New_York','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Eastern Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Chicago','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Central Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Denver','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mountain Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Phoenix','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mountain Standard Time' AND AIRPORTS.AIRPORT_CITY LIKE '%%AZ%%' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/La_Paz','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mexican Pacific Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Los_Angeles','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Pacific Standard Time' \
+			ORDER BY UPCOMING_FLIGHTS.DEPART_DATE_TIME ASC" % (currentUtcDateTime,currentUtcDateTime,currentUtcDateTime,currentUtcDateTime,currentUtcDateTime,currentUtcDateTime,currentUtcDateTime)
+	try:
+		cursor.execute(sql)
+		results = cursor.fetchone()
+		if str(results[0]) != "None":
+			reservedFlightIdArray = results[0].split(',')
+			for reservedFlightId in reservedFlightIdArray:
+				sql = "DELETE FROM RESERVED_FLIGHTS WHERE RESERVED_FLIGHTS.RESERVED_FLIGHT_ID='%s'" % (reservedFlightId)
+				try:
+					cursor.execute(sql)
+					db.commit()
+				except:
+					db.rollback()
+					logF = open(logFile, "a")
+					logMessage = "%s ERROR: Unable to delete reserved flight [id:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
+					logF.write(logMessage)
+					logF.close()
+		if str(results[1]) != "None":
+			upcomingFlightIdArray = results[1].split(',')
+			for upcomingFlightId in upcomingFlightIdArray:
+				sql = "DELETE FROM UPCOMING_FLIGHTS WHERE UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID='%s'" % (upcomingFlightId)
+				try:
+					cursor.execute(sql)
+					db.commit()
+				except:
+					db.rollback()
+					logF = open(logFile, "a")
+					logMessage = "%s ERROR: Unable to delete past flight [id:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),upcomingFlightId)
+					logF.write(logMessage)
+					logF.close()
+	except:
+		logF = open(logFile, "a")
+		logMessage = "%s ERROR: Unable to select past flights to delete\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
+		logF.write(logMessage)
+		logF.close()
+		db.close()
+		sys.exit(1)
+
+	print ""
+	#####################################################################
+	## Retrieve list of upcoming reserved flights
+	#####################################################################
+	sql = "SELECT RESERVED_FLIGHTS.RESERVED_FLIGHT_ID,RESERVED_FLIGHTS.EMAIL,RESERVED_FLIGHTS.PHONE_NUM,WIRELESS_CARRIERS.CARRIER_TEXT_EMAIL,RESERVED_FLIGHTS.CONFIRMATION_NUM,UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE,UPCOMING_FLIGHTS.ARRIVE_AIRPORT_CODE,UPCOMING_FLIGHTS.DEPART_DATE_TIME,UPCOMING_FLIGHTS.FLIGHT_NUM,AIRPORTS.AIRPORT_TIMEZONE \
 			FROM RESERVED_FLIGHTS \
 			LEFT JOIN UPCOMING_FLIGHTS \
 			ON UPCOMING_FLIGHTS.UPCOMING_FLIGHT_ID=RESERVED_FLIGHTS.UPCOMING_FLIGHT_ID \
 			LEFT JOIN WIRELESS_CARRIERS \
 			ON RESERVED_FLIGHTS.WIRELESS_CARRIER_ID=WIRELESS_CARRIERS.WIRELESS_CARRIER_ID \
-			WHERE UPCOMING_FLIGHTS.DEPART_DATE_TIME<='%s' AND RESERVED_FLIGHTS.CHECKIN_ALERT=0" % (checkinAlertDateTime)
+			LEFT JOIN AIRPORTS \
+			ON UPCOMING_FLIGHTS.DEPART_AIRPORT_CODE=AIRPORTS.AIRPORT_CODE \
+			WHERE CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Aruba','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Atlantic Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/New_York','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Eastern Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Chicago','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Central Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Denver','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mountain Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Phoenix','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mountain Standard Time' AND AIRPORTS.AIRPORT_CITY LIKE '%%{AZ}%%' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/La_Paz','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Mexican Pacific Standard Time' \
+			OR CONVERT_TZ(UPCOMING_FLIGHTS.DEPART_DATE_TIME,'America/Los_Angeles','+00:00')<='%s' AND AIRPORTS.AIRPORT_TIMEZONE='Pacific Standard Time' \
+			AND RESERVED_FLIGHTS.CHECKIN_ALERT=0 ORDER BY UPCOMING_FLIGHTS.DEPART_DATE_TIME ASC" % (checkinAlertDateTime,checkinAlertDateTime,checkinAlertDateTime,checkinAlertDateTime,checkinAlertDateTime,checkinAlertDateTime,checkinAlertDateTime)
 	try:
 		cursor.execute(sql)
 		results = cursor.fetchall()
 		for row in results:
+			print row
 			reservedFlightId = row[0]
 			email = str(row[1])
 			phoneNum = str(row[2])
@@ -83,27 +152,28 @@ def main():
 			arriveAirportCode = row[6]
 			departDateTime = row[7].strftime('%a %b %d %I:%M%p')
 			flightNum = row[8]
+			timezone = row[9]
 			if email == "None":
 				notificationAddress = "%s%s" % (phoneNum,textEmail)
 			else:
 				notificationAddress = email
 			# print "%s %s %s %s %s %s" % (notificationAddress,confirmationNum,departAirportCode,arriveAirportCode,departDateTime,flightNum)
-			if not sendCheckinAlert(notificationAddress,confirmationNum,departAirportCode,arriveAirportCode,departDateTime,flightNum):
-				try:
-					sql = "UPDATE RESERVED_FLIGHTS SET CHECKIN_ALERT='%s',CHECKIN_ALERT_TIMESTAMP='%s' WHERE RESERVED_FLIGHT_ID='%s'" % ('1',time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
-					cursor.execute(sql)
-					db.commit()
-				except:
-					db.rollback()
-					logF = open(logFile, "a")
-					logMessage = "%s ERROR: Unable to update checkin alert flag [reservedFlightId:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
-					logF.write(logMessage)
-					logF.close()
-			else:
-				logF = open(logFile, "a")
-				logMessage = "%s ERROR: Unable to send checkin alert [reservedFlightId:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
-				logF.write(logMessage)
-				logF.close()
+			# if not sendCheckinAlert(notificationAddress,confirmationNum,departAirportCode,arriveAirportCode,departDateTime,flightNum):
+			# 	try:
+			# 		sql = "UPDATE RESERVED_FLIGHTS SET CHECKIN_ALERT='%s',CHECKIN_ALERT_TIMESTAMP='%s' WHERE RESERVED_FLIGHT_ID='%s'" % ('1',time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
+			# 		cursor.execute(sql)
+			# 		db.commit()
+			# 	except:
+			# 		db.rollback()
+			# 		logF = open(logFile, "a")
+			# 		logMessage = "%s ERROR: Unable to update checkin alert flag [reservedFlightId:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
+			# 		logF.write(logMessage)
+			# 		logF.close()
+			# else:
+			# 	logF = open(logFile, "a")
+			# 	logMessage = "%s ERROR: Unable to send checkin alert [reservedFlightId:%s]\n" % (time.strftime("%Y-%m-%d %H:%M:%S"),reservedFlightId)
+			# 	logF.write(logMessage)
+			# 	logF.close()
 	except:
 		logF = open(logFile, "a")
 		logMessage = "%s ERROR: Unable to select reserved flights for checkin\n" % (time.strftime("%Y-%m-%d %H:%M:%S"))
