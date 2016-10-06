7a896366dc6a510966a6cee09c7de25a7e4cfa5a
lojavier <loj90@sbcglobal.net>
2016-10-06 15:57:19 -0700

    Update 10/06/2016

--- setup/update.sh
+++ setup/update.sh
@@ -1,59 +1,38 @@
 #!/bin/bash
 
 SW_PATH=/home/pi/swfarereducer
-UPDATE_PATH=$SW_PATH/update
+APP_PATH=$SW_PATH/app
 LOG_PATH=$SW_PATH/logs/sys.log
 WEB_PATH=/var/www
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') info update.sh: Starting update..." >> $LOG_PATH
 
 cd $SW_PATH
-git checkout .
+git pull
 if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not checkout files from git" >> $LOG_PATH
+	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not update files from GitHub" >> $LOG_PATH
 	exit 1
 fi
 
-sudo mkdir -p $UPDATE_PATH
+diff -r $APP_PATH $WEB_PATH
 if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not create $UPDATE_PATH" >> $LOG_PATH
+	echo "$(date +'%Y-%m-%d %H:%M:%S') info update.sh: App update detected" >> $LOG_PATH
+else
+	echo "$(date +'%Y-%m-%d %H:%M:%S') info update.sh: No app update available" >> $LOG_PATH
+	exit 0
+fi
+
+rm -rf $WEB_PATH/*
+if [ $? -ne 0 ]; then
+	echo "$(date +'%Y-%m-%d %H:%M:%S') warning update.sh: Could not remove all $WEB_PATH files" >> $LOG_PATH
+fi
+
+cp -rf $APP_PATH/* $WEB_PATH/.
+if [ $? -ne 0 ]; then
+	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not copy $APP_PATH files into $WEB_PATH" >> $LOG_PATH
 	exit 1
 fi
 
-sudo mv -f $SW_PATH/css $UPDATE_PATH/.
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not update CSS files" >> $LOG_PATH
-	exit 1
-fi
-
-sudo mv -rf $SW_PATH/css /var/www/css
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not update CSS files" >> $LOG_PATH
-	exit 1
-fi
-
-sudo mv -f $SW_PATH/*.php /var/www/.
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not update PHP files" >> $LOG_PATH
-	exit 1
-fi
-
-sudo rm -rf /var/www/.
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not remove all $WEB_PATH files" >> $LOG_PATH
-	exit 1
-fi
-
-sudo mv -f $SW_PATH/update/* $WEB_PATH/.
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not update website files" >> $LOG_PATH
-	exit 1
-fi
-
-sudo rm -rf $UPDATE_PATH
-if [ $? -ne 0 ]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') error update.sh: Could not create $UPDATE_PATH" >> $LOG_PATH
-	exit 1
-fi
+echo "$(date +'%Y-%m-%d %H:%M:%S') info update.sh: Update successful!" >> $LOG_PATH
 
 sync
